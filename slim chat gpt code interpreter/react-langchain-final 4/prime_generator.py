9612454b7f54aaedc6afcd8c5767e9375f231281
chetan009-sjce <76608950+chetan009-sjce@users.noreply.github.com>
2025-07-25 19:57:04 +0530

    Add files via upload

---
+++ slim chat gpt code interpreter/react-langchain-final 4/prime_generator.py
@@ -0,0 +1,10 @@
+
+# This script prints all the prime numbers from 1 to 100.
+for num in range(2, 101):
+    # all prime numbers are greater than 1
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
